8211b3ef0f7c82829b234516f2721f74c8b35da3
FabioChanM <99096561+FabioChanM@users.noreply.github.com>
2022-02-15 10:33:11 -0600

    Add files via upload

---
+++ Kata1.py
@@ -0,0 +1,9 @@
+
+from datetime import date
+date.today()
+print("Today's date is: ")
+print(date.today())
+parsec = 11
+lightyears = 3.26156 * parsec
+print(str(parsec) + " parsec, is " + str(lightyears) + " lightyears")
+py
